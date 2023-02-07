aa46f18bd18c643d1d43455015fe683c6c1ac9e3
frandue <fran.stix@gmail.com>
2023-02-07 22:48:04 +0100

    A falta del opcional y de una revision del resto

---
+++ M_1_HTML_INTRO/3_entregas/4-Interacción con el DOM  JavaScript/4_/JS/demo.js
@@ -0,0 +1,5 @@
+document.getElementById("name").value = "Blanquita";
+document.getElementById("lastName").value = "Javascriptez";
+document.getElementById("PhoneNumber").value = "+69 111222333";
+/* Añado la imagen del Avatar para que la cargue la pagina*/
+document.getElementById("avatar").src = "img/ruta_imagen.png";
