e40c4d1ed42ad20e9e0dde88fa723c29465af019
frandue <fran.stix@gmail.com>
2023-02-19 21:27:21 +0100

    Opcional_Incluido

--- M_2_HTML_var&dat/3_entregas/3_Opcional/1_Varios/JS/app.js
+++ M_2_HTML_var&dat/3_entregas/3_Opcional/1_Varios/JS/app.js
@@ -1,14 +1,48 @@
 // Objetivo 1 [Datos del hotel]
-var hotel = {
-  name: "Mencey",
-  location: "Tenerife",
-  img:
-    "https://media-cdn.tripadvisor.com/media/photo-m/1280/17/2f/e9/d7/iberostar-grand-mencey.jpg",
+//var hotel = {
+// name: "Mencey",
+//  location: "Tenerife",
+//  img:
+//    "https://media-cdn.tripadvisor.com/media/photo-m/1280/17/2f/e9/d7/iberostar-grand-mencey.jpg",
+//};
+//document.getElementById("name-hotel").innerHTML = "Hotel " + hotel.name;
+//document.getElementById("location-hotel").innerHTML =
+//  "Ubicado en " + hotel.location;
+//document.getElementById("img-hotel").src = hotel.img;
+
+var hoteles = {
+  Mencey: {
+    name: "Mencey",
+    location: "Tenerife",
+    img:
+      "https://media-cdn.tripadvisor.com/media/photo-m/1280/17/2f/e9/d7/iberostar-grand-mencey.jpg",
+  },
+  Lani: {
+    name: "Lani's Suites Deluxe",
+    location: "Lanzarote",
+    img:
+      "https://media-cdn.tripadvisor.com/media/photo-m/1280/17/49/47/e3/lani-s-suites-deluxe.jpg",
+  },
+  Occidental: {
+    name: "Occidental",
+    location: "Isla Cristina",
+    img:
+      "https://media-cdn.tripadvisor.com/media/photo-m/1280/17/49/47/e3/lani-s-suites-deluxe.jpg",
+},
 };
-document.getElementById("name-hotel").innerHTML = "Hotel " + hotel.name;
+
+var selectedHotel = prompt(
+  "Indique hotel sobre el que quiere hacer la reseña: Mencey , Lani o Occidental",
+);
+// diff
+document.getElementById("name-hotel").innerHTML =
+  "Hotel " + hoteles[selectedHotel].name;
+
 document.getElementById("location-hotel").innerHTML =
-  "Ubicado en " + hotel.location;
-document.getElementById("img-hotel").src = hotel.img;
+  "Ubicado en " + hoteles[selectedHotel].location;
+
+document.getElementById("img-hotel").src = hoteles[selectedHotel].img;
+// diff
 
 // Objetivo 2 [Puntuación del hotel]
 var rating = prompt("Puntuación: del 1 al 5");
