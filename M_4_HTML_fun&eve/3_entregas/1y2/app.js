d46b51444754cd86c0da8442314f7290d7bb6d5a
frandue <fran.stix@gmail.com>
2023-02-28 23:08:14 +0100

    Botones_calculadora

---
+++ M_4_HTML_fun&eve/3_entregas/1y2/app.js
@@ -0,0 +1,44 @@
+//Funcion suma(al poner un + me los concatenaba)
+function suma (a,b){
+ return (-1*(-1*a - 1*b)) ;
+};
+//Funcion resta
+ function resta (a,b){
+ return (a - b) ;
+};
+//Funcion multiplicacion
+function multiplicacion (a,b){
+ return (a * b) ;
+};
+//Funcion divisio
+function division (a,b){
+ return (a / b) ;
+};    
+//Prgunto que operacion se quiere realizar     
+const opera = prompt ("Que operacion quieres : suma, resta, multiplicacion o division");
+console.log(opera);
+
+const varA = prompt ("Valor del primer numero");
+
+const varB = prompt ("Valor del segundo numero");
+
+var resultado = 0
+
+switch (opera){
+case "suma" :
+        resultado = suma (varA,varB);
+        console.log("El valor de la suma "+ resultado);
+        break;
+case "resta" :
+        resultado = resta (varA,varB);
+        console.log("El valor de la resta "+ resultado);
+        break;
+case "multiplicacion" :
+        resultado = multiplicacion (varA,varB);
+        console.log("El valor de la multiplicacion "+ resultado);
+        break;
+case "division" :
+        resultado = division (varA,varB);
+        console.log("El valor de la division "+ resultado);
+        break;       
+};
