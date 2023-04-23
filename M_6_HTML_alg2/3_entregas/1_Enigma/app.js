60062bea66e90bb6e6022499f5aa4e2aaf168fd8
frandue <fran.stix@gmail.com>
2023-04-23 22:36:36 +0200

    enigma_comentado

--- M_6_HTML_alg2/3_entregas/1_Enigma/app.js
+++ M_6_HTML_alg2/3_entregas/1_Enigma/app.js
@@ -4,14 +4,17 @@ var encryptedAlphabet = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm"; //sz*
 
 //funcion de encriptado
 function encryptMessage() {
-  var message = document.getElementById("message").value;   // creo una variable en la que se almacene el texto a encriptar
+  var message = document.getElementById("message").value;   // creo una variable en la que se almacene el texto a encriptar que he introducido
   var encryptedMessage = "";    // creo una variable en la que se almacene el texto encriptado
-  for (var i = 0; i < message.length; i++) {
-    var index = plainAlphabet.indexOf(message[i]);
-    if (index != -1) {
+  for (var i = 0; i < message.length; i++) {    // Recorro los elementos de "message" desde 0 hasta message.length
+    var index = plainAlphabet.indexOf(message[i]);    // De cada letra de "message" busco el numero que le corresponde en "plainAlphabet"
+    console.log(index);
+    if (index != -1) {  // Si el numero de index es != a -1 voy almaceando en "encryptedMessage" el mensaje encriptado 
       encryptedMessage += encryptedAlphabet[index];
-    } else {
+      console.log(encryptedMessage);
+    } else {    // Si el numero de index es = a -1 guado el valor "message" en "encryptedMessage"
       encryptedMessage += message[i];
+      console.log(encryptedMessage);
     }
   }
   document.getElementById("encrypted").value = encryptedMessage;
