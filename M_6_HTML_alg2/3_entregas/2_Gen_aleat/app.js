721af7fa5f01892c2f67151d826f78cb7e00b87e
frandue <fran.stix@gmail.com>
2023-04-27 20:03:05 +0200

    Generador_aleatorio

--- M_6_HTML_alg2/3_entregas/2_Gen_aleat/app.js
+++ M_6_HTML_alg2/3_entregas/2_Gen_aleat/app.js
@@ -1,24 +1,28 @@
 
 
 var randomPick = (numN, nMin, nMax) => {
-  var numN = document.getElementById("varA").value;
-  var nMin = document.getElementById("varB").value;
-  var nMax = document.getElementById("varC").value;
+  var numN = parseInt(document.getElementById("varA").value);
+  var nMin = parseInt(document.getElementById("varB").value);
+  var nMax = parseInt(document.getElementById("varC").value);
 
-  var arrayRandom = "";
-    if( nMin > nMax) {
-      alert("El número mínimo no puede ser mayor que el maximo " );
+  var numRandom = 0;
+  const arrayRandom = [];
+    if( (nMin+numN) > nMax) {   // Condiciones varias que yo considero
+      alert("Aumenta el número max " );
     } else if( nMin == nMax) {
       alert("El número max no puede ser igual al número min" );
-    } else if( numN < 2) {
-      alert("El número a generado tiene que ser mayor de 2" );
     } else if( nMax > nMin ){
         for (var i = 0; i < numN; i++) {
-          var numRandom = ((nMax - nMin)/Math.random()) + nMin;
-          arrayRandom += numRandom.toFixed();
-          console.log(numRandom);
+          numRandom = Math.round(((nMax - nMin)*Math.random()) + nMin);
+          while ( arrayRandom.includes(numRandom)){     //Que esta operacion se realice mientras el numero ya pertenezca al array
+            numRandom = Math.round(((nMax - nMin)*Math.random()) + nMin);
+          }
+          arrayRandom.push(numRandom);    //Voy añadiendo los numeros al array   
     }
+    console.log(arrayRandom);
     }
+    const arrayRan = document.getElementById("arrayRan");
+    arrayRan.innerHTML = `Los valores aleatorios son: ${arrayRandom}`;
   }
 //EVENTS
 document.getElementById("generator").addEventListener("click", randomPick);
