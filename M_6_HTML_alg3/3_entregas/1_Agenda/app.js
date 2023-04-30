f38390f757d16eeae69cb5f86de01d8dc5fe7eb3
frandue <fran.stix@gmail.com>
2023-04-30 22:22:10 +0200

    Agenda_completa

--- M_6_HTML_alg3/3_entregas/1_Agenda/app.js
+++ M_6_HTML_alg3/3_entregas/1_Agenda/app.js
@@ -10,7 +10,6 @@ var WORK_HOURS = [
   "15:00 - 16:00",
   "16:00 - 17:00"
 ];
-
 // Datos
 var myTeam = [
   {
@@ -30,9 +29,7 @@ var myTeam = [
     availability: new Array(8).fill(true)
   },
 ];
-
 //Generador aleatorio de siponiblidad
-
 for(var i = 0; i < myTeam.length; i++) {    //Recorro a todos los participante del equipo
   console.log("La disponibilidad de "+ myTeam[i].name);
   for(var j = 0; j < WORK_HOURS.length; j++) {    //Recorro todas las horas con cada miembro
@@ -40,4 +37,19 @@ for(var i = 0; i < myTeam.length; i++) {    //Recorro a todos los participante d
     myTeam[i].availability[j] = randomAvailability;   // Almaceno la disponibilidad
     console.log("   "+ WORK_HOURS[j]+"  "+randomAvailability);
   }
+}
+//Busqueda de hueco libre
+var personsFree = new Array(WORK_HOURS.length).fill(0);   // Inicializamos el array con ceros, la longitud es el numero de horas
+for (var i = 0; i < myTeam.length; i++) {                 // Voy recorriendo cada persona del equipo
+  for (var j = 0; j < WORK_HOURS.length; j++) {           //Voy recorriendo cada hora
+    if (myTeam[i].availability[j] == "Si") {
+      personsFree[j] += 1;                                // Incrementamos el valor de personsFree para el índice j
+    }
+  }
+}
+var index = parseInt(personsFree.indexOf(4));
+if (index != -1) {
+  console.log("Hueco encontrado en el horario " + WORK_HOURS[index]);
+} else {
+  console.log("Lo siento. No hay hueco disponible en el equipo.");  
 }
