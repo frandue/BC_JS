9c4689e0b6810a45f7ed6fcbcba6791e472ca672
frandue <fran.stix@gmail.com>
2023-05-08 16:26:38 +0200

    actualizado_cambio

--- M_6_HTML_alg3/3_entregas/2_Cambio/app.js
+++ M_6_HTML_alg3/3_entregas/2_Cambio/app.js
@@ -5,14 +5,14 @@ const changeCost = [200,100,50,20,10,5,2,1,0.5,0.2,0.1,0.05,0.02,0.01];
 function billTicket () {
   var deuda = parseFloat(document.getElementById("cost").value).toFixed(2);
   var billeteSeleccionado = parseFloat(document.getElementById("cash").value).toFixed(2);
-    if(deuda > billeteSeleccionado && billeteSeleccionado < 200 && deuda < 200) {
-       alert("Pagar con un billete mayor");
+  var cambio = (billeteSeleccionado - deuda).toFixed(2);
+    if(cambio < 0) {
+       alert("Necesitas mas dinero");
     } else if (deuda == billeteSeleccionado) {
        alert("El pago fue exacto");  
     } else if (isNaN(deuda) || deuda == "0") {
        alert("No debes nada");
     } else {  
-       var cambio = (billeteSeleccionado - deuda).toFixed(2);
        var resultado = document.getElementById("resultado");
        resultado.innerHTML = "";
        for (var i = 0; i < changeCost.length; i++) {
