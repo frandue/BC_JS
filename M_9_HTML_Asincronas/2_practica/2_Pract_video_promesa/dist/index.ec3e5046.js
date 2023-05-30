737a625cd5994db5b1ab41922bad89983be13c70
frandue <fran.stix@gmail.com>
2023-05-30 12:18:32 +0200

    practicas asincronas

--- M_9_HTML_Asincronas/2_practica/2_Pract_video_promesa/dist/index.ec3e5046.js
+++
@@ -1,9 +0,0 @@
-const myPromise = new Promise((resolve, reject)=>{
-    //Podemos hacer todos los calculos que queramos en el cuerpo de la promesa
-    resolve("Hola, esto es la respuesta correcta de la promesa ;)");
-});
-myPromise.then(function(result) {
-    alert(result);
-});
-
-//# sourceMappingURL=index.ec3e5046.js.map
